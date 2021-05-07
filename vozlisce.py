093f7fde18f1973e1354fd9b42cebaf4852ded33
ZigaPe <73107408+ZigaPe@users.noreply.github.com>
2021-05-07 21:37:59 +0200

    dodaj funkciji noter

--- vozlisce.py
+++ vozlisce.py
@@ -15,17 +15,18 @@ idx = 0
 while True:
     car_speed = funkcije_avta.getSpeed()
     car_gear = funkcije_avta.getGear()
-    
     car_position = funkcije_avta.getPosition()
+    car_orientation = funkcije_avta.getOrientation()
 
     f.write("hitrost: %d, prestava: %d\n" % (car_speed, car_gear))
-    f.write("Polozaj avta\n:" %(car_position))
-    print("hitrost: %d, prestava: %d" % (car_speed, car_gear))
+    f.write(car_position)
+    f.write(car_orientation)
     
-    #tukaj je tako, ker je oblika izpisa malo nenavadna
-    print("POLOZAJ AVTA: ----------------------------")
+    
+    print("hitrost: %d, prestava: %d" % (car_speed, car_gear))
+    print(car_orientation)
     print(car_position)
-    print("------------------------------------------")
+    
 
     # trenutno se shranita slika scene in globinska slika
     funkcije_avta.getImages(idx)
