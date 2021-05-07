9f9b7dfce85e4ac25fa2c78e88082ec0ebd1a5d7
ZigaPe <73107408+ZigaPe@users.noreply.github.com>
2021-05-07 20:30:06 +0200

    vkljucil polozaj avta

--- vozlisce.py
+++ vozlisce.py
@@ -15,9 +15,17 @@ idx = 0
 while True:
     car_speed = funkcije_avta.getSpeed()
     car_gear = funkcije_avta.getGear()
+    
+    car_position = funkcije_avta.getPosition()
 
     f.write("hitrost: %d, prestava: %d\n" % (car_speed, car_gear))
+    f.write("Polozaj avta\n:" %(car_position))
     print("hitrost: %d, prestava: %d" % (car_speed, car_gear))
+    
+    #tukaj je tako, ker je oblika izpisa malo nenavadna
+    print("POLOZAJ AVTA: ----------------------------")
+    print(car_position)
+    print("------------------------------------------")
 
     # trenutno se shranita slika scene in globinska slika
     funkcije_avta.getImages(idx)
