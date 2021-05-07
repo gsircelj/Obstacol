d81fed9a5594018ff26f9cdea6d09cdc8e454abc
Gasper <gasper.sircelj@student.um.si>
2021-05-07 13:11:16 +0200

    Dodana funkcija getImages()

--- funkcije_avta.py
+++ funkcije_avta.py
@@ -5,12 +5,24 @@
 
 
 import airsim
+import cv2
+import numpy as np
+import os
+import tempfile
 
 # connect to the AirSim simulator
 client = airsim.CarClient()
 client.confirmConnection()
-client.enableApiControl(True)
 car_controls = airsim.CarControls()
+
+# naredi nov direktorij za shranjevanje slik
+tmp_dir = os.path.join("", "airsim_car")
+print ("Saving images to %s" % tmp_dir)
+try:
+    os.makedirs(tmp_dir)
+except OSError:
+    if not os.path.isdir(tmp_dir):
+        raise
 
 def getSpeed():
     car_state = client.getCarState()
@@ -20,3 +32,26 @@ def getGear():
     car_state = client.getCarState()
     return car_state.gear
 
+def getImages(idx):
+    # get camera images from the car
+    responses = client.simGetImages([
+        airsim.ImageRequest("0", airsim.ImageType.DepthVis),  #depth visualization image
+        airsim.ImageRequest("1", airsim.ImageType.Scene)]) #scene vision image in png format
+    #scene vision image in uncompressed RGB array
+    print('Retrieved images: %d' % len(responses))
+
+    for response_idx, response in enumerate(responses):
+        filename = os.path.join(tmp_dir, f"{idx}_{response.image_type}_{response_idx}")
+	
+	
+        if response.pixels_as_float:
+            print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
+            airsim.write_pfm(os.path.normpath(filename + '.pfm'), airsim.get_pfm_array(response))
+        elif response.compress: #png format
+            print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
+            airsim.write_file(os.path.normpath(filename + '.png'), response.image_data_uint8)
+        else: #uncompressed array
+            print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
+            img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8) # get numpy array
+            img_rgb = img1d.reshape(response.height, response.width, 3) # reshape array to 3 channel image array H X W X 3
+            cv2.imwrite(os.path.normpath(filename + '.png'), img_rgb) # write to png
--- vozlisce.py
+++ vozlisce.py
@@ -9,19 +9,27 @@ import funkcije_avta
 
 f = open("izpis_podatkov.txt", "w")
 
+# shranjevanje trenutne iteracije
+idx = 0
+
 while True:
     car_speed = funkcije_avta.getSpeed()
     car_gear = funkcije_avta.getGear()
+
     f.write("hitrost: %d, prestava: %d\n" % (car_speed, car_gear))
+    print("hitrost: %d, prestava: %d" % (car_speed, car_gear))
+
+    # trenutno se shranita slika scene in globinska slika
+    funkcije_avta.getImages(idx)
     
     # tako dobimo manj podatkov in s tem preglednejši izgled podatkov
     # v prihajajočih nadgradnjah to izbriši
-    time.sleep(0.5)
+    time.sleep(1)
 
+    idx += 1
+
+    print("\n")
 f.close()
-
-
-# In[ ]:
 
 
 
